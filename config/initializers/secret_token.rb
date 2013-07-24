1c3d9813004d12ad021a3041b1c87ad41f89bb28
abhinav <abhinavsoni9@gmail.com>
2013-07-24 11:15:47 +0530

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventalic1::Application.config.secret_token = '7850b185bd7937f8ed673fd6173a0ea32ae517df0cca8b076349405679c6c3ffc0c479f1cf2abcf7b2a8bb2bf93464c7ee8e5fc361d7c5b9cf6406217ac588e4'
